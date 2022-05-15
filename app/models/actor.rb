260853ffd6fc1047400159553df5ef50c8862cf6
Julian Anaya <juliananaya531@gmail.com>
2022-05-14 18:56:36 -0700

    w3d4 hw

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,8 @@
 class Actor < ApplicationRecord
+    validates :first_name, presence: true
+    validates :first_name, length: { in: 2..100}
+    validates :last_name, presence: true
+    validates :last_name, length: { in: 2..100}
+    validates :known_for, presence: true
+    validates :age, numericality: {greater_than: 13 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
-class Movie < ApplicationRecord  
+class Movie < ApplicationRecord
+    validates :title, presence: true
+    validates :title, length: { in: 2..100}
+    validates :known_for, presence: true
+    validates :known_for, length { in: 10..300}
+    validates :year, presence: true
+    validates :year, numericality: {greater_than: 1800 }
 end
